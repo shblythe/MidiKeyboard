b10433da2369ebf9af4aa1754e4c4b9f6c74749a
Stephen Blythe <stephen@urwick.co.uk>
2020-04-24 21:35:07 +0100

    Stage 2. Display note names

--- MidiKeyboard.ino
+++ MidiKeyboard.ino
@@ -1,7 +1,13 @@
-#define NUMROWS 8
+#define NUMROWS 4
 #define NUMCOLS 8
 const byte rows[]={53,49,45,41,51,47,43,39};
 const byte cols[]={52,50,48,46,44,42,40,38};
+
+const char* note_names[]={
+  "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
+  "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
+  "C3"
+};
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,11 +33,13 @@ void loop() {
     digitalWrite(cols[c],HIGH);
     //delay(1);
     for (int r=0; r<NUMROWS; r++)
+    {
       if (digitalRead(rows[r]))
       {
-        Serial.print(r);
+        Serial.print(note_names[r*NUMCOLS+c]);
         Serial.print(" ");
-        Serial.println(c);
       }
+    }
   }
+  Serial.println();
 }
