c5dca98e5f355f7d4b59b7eaa48de0c7fcd20561
Stephen Blythe <stephen@urwick.co.uk>
2020-04-24 21:40:37 +0100

    Now sends midi notes

--- MidiKeyboard.ino
+++ MidiKeyboard.ino
@@ -1,13 +1,20 @@
+#include <MIDI.h>
 #define NUMROWS 4
 #define NUMCOLS 8
 const byte rows[]={53,49,45,41,51,47,43,39};
 const byte cols[]={52,50,48,46,44,42,40,38};
+const int middleC=0x30;
+
+byte switch_states[NUMROWS*NUMCOLS];
+byte old_switch_states[NUMROWS*NUMCOLS];
 
 const char* note_names[]={
   "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
   "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
   "C3"
 };
+
+MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial,Serial1,MIDI,midi::DefaultSettings);
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,6 +26,8 @@ void setup() {
   } 
   for (int i=0; i<NUMROWS; i++)
     pinMode(rows[i],INPUT);
+  memset(old_switch_states,LOW,NUMROWS*NUMCOLS);
+  MIDI.begin();
 }
 
 void loop() {
@@ -33,12 +42,19 @@ void loop() {
     digitalWrite(cols[c],HIGH);
     //delay(1);
     for (int r=0; r<NUMROWS; r++)
+      switch_states[r*NUMCOLS+c]=digitalRead(rows[r]);
+  }
+  for (int i=0; i<NUMROWS*NUMCOLS; i++)
+  {
+    if (switch_states[i]==HIGH)
     {
-      if (digitalRead(rows[r]))
-      {
-        Serial.print(note_names[r*NUMCOLS+c]);
-        Serial.print(" ");
-      }
+      Serial.print(note_names[i]);
+      Serial.print(" ");
+    }
+    if (switch_states[i]!=old_switch_states[i])
+    {
+      MIDI.sendNoteOn(middleC+i,(switch_states[i]==HIGH)?64:0,1);
+      old_switch_states[i]=switch_states[i];
     }
   }
   Serial.println();
